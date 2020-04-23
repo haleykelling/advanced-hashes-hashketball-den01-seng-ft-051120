ae0d976865ceb8bc4918e9c9957cb94235d0d5fb
Haley Kelling <haley@kellings.com>
2020-04-23 20:35:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -132,7 +132,7 @@ end
 def num_points_scored(player)
   game_hash.each do |team|
     team[1][:players].each do |player_info|
-      p player_info
+      p player_info[:player_name]
     end
   end
 end
